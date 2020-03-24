2e455c56b8c588229c53f3c9781ce55c1b031b6f
Семёнова Анна <semenova@ACTIMIND>
2020-03-24 12:37:15 +0300

    to class

--- 2-input-history-changes/script.js
+++ 2-input-history-changes/script.js
@@ -1,60 +1,72 @@
 "use strict";
 
-(function() {
-  function setInput(el) {
-    const input = el.querySelector("input");
-    const buttonPrev = el.querySelector(".js-button-prev");
-    const buttonNext = el.querySelector(".js-button-next");
+class InputWithHistory {
+  constructor(el) {
+    this.input = el.querySelector("input");
+    this.buttonPrev = el.querySelector(".js-button-prev");
+    this.buttonNext = el.querySelector(".js-button-next");
 
-    let curPosition = 0;
-    let history = [];
+    this.curPosition = 0;
+    this.history = [];
 
-    buttonPrev.setAttribute("disabled", "disabled");
-    buttonNext.setAttribute("disabled", "disabled");
+    this.disabledButton([this.buttonPrev, this.buttonNext]);
+    this.bindEvents();
+  }
 
-    const pushToHistory = value => {
-      history.push(value);
-      curPosition = history.length - 1;
-      buttonPrev.removeAttribute("disabled");
-      buttonNext.setAttribute("disabled", "disabled");
-    };
+  enabledButton(arr) {
+    arr.forEach(el => el.removeAttribute("disabled"));
+  }
 
-    const goPrev = () => {
-      if (curPosition > 0) {
-        curPosition--;
-        input.value = history[curPosition];
-        buttonNext.removeAttribute("disabled");
-      } else {
-        input.value = "";
-        buttonPrev.setAttribute("disabled", "disabled");
-      }
-    };
+  disabledButton(arr) {
+    arr.forEach(el => el.setAttribute("disabled", "disabled"));
+  }
 
-    const goNext = () => {
-      if (curPosition < history.length - 1) {
-        curPosition++;
-        input.value = history[curPosition];
-        buttonPrev.removeAttribute("disabled");
-      } else {
-        buttonNext.setAttribute("disabled", "disabled");
-      }
-    };
-
-    input.addEventListener("input", evt => {
+  bindEvents() {
+    this.input.addEventListener("input", evt => {
       evt.preventDefault();
-      pushToHistory(evt.target.value);
+      this.pushToHistory(evt.target.value);
     });
 
-    buttonPrev.addEventListener("click", evt => {
+    this.buttonPrev.addEventListener("click", evt => {
       evt.preventDefault();
-      goPrev();
+      this.goPrev();
     });
 
-    buttonNext.addEventListener("click", evt => {
+    this.buttonNext.addEventListener("click", evt => {
       evt.preventDefault();
-      goNext();
+      this.goNext();
     });
   }
 
-  document.querySelectorAll(".js-input-wrapper").forEach(el => setInput(el));
-})();
+  pushToHistory(value) {
+    this.history.push(value);
+    this.curPosition = this.history.length - 1;
+    this.enabledButton([this.buttonPrev]);
+    this.disabledButton([this.buttonNext]);
+  }
+
+  goPrev() {
+    if (this.curPosition > 0) {
+      this.curPosition--;
+      this.input.value = this.history[this.curPosition];
+      this.enabledButton([this.buttonNext]);
+    } else {
+      this.input.value = "";
+      this.disabledButton([this.buttonPrev]);
+    }
+  }
+
+  goNext() {
+    if (this.curPosition < this.history.length - 1) {
+      this.curPosition++;
+      this.input.value = this.history[this.curPosition];
+      this.enabledButton([this.buttonPrev]);
+    } else {
+      this.disabledButton([this.buttonNext]);
+    }
+  }
+}
+
+document
+  .querySelectorAll(".js-input-wrapper")
+  .forEach(el => new InputWithHistory(el));
