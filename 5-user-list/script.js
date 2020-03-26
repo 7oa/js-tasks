897e037d6ac419eb73921eeee41fdf75930ab084
Семёнова Анна <semenova@ACTIMIND>
2020-03-26 10:39:00 +0300

    to class

--- 5-user-list/script.js
+++ 5-user-list/script.js
@@ -1,50 +1,104 @@
 "use strict";
 
-(function() {
-  const form = document.querySelector("form");
-  const table = document.querySelector("table");
-  const tbody = document.querySelector("tbody");
+class UsersList {
+  constructor(form, table) {
+    this.users = new Map();
+    this.form = form;
+    this.table = table;
+    this.tbody = table.querySelector("tbody");
+    this.bindEvents();
+  }
 
-  table.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    let target = evt.target;
-    if (target.classList.contains("js-button-activate")) {
-      let columnActivated = [
-        ...target.parentNode.parentNode.children
-      ].filter(el => el.classList.contains("js-active-value"))[0];
-      switch (target.innerHTML) {
-        case "Activate":
-          columnActivated.innerText = "true";
-          target.innerHTML = "Deactivate";
-          break;
-        case "Deactivate":
-          columnActivated.innerText = "false";
-          target.innerHTML = "Activate";
-          break;
+  bindEvents() {
+    this.table.addEventListener("click", evt => {
+      evt.preventDefault();
+      const target = evt.target;
+      const userID = target.getAttribute("data-id");
+      if (target.classList.contains("js-button-activate")) {
+        this.toggleActivate(userID);
       }
-    }
-    if (target.classList.contains("js-button-remove")) {
-      target.parentNode.parentNode.remove();
-    }
-    if (target.classList.contains("js-value")) {
-      target.innerText = prompt("Add new value", target.innerText);
-    }
-  });
+      if (target.classList.contains("js-button-remove")) {
+        this.remove(userID);
+      }
+      if (target.classList.contains("js-value")) {
+        const value = prompt("Add new value", target.innerText);
+        const field = target.getAttribute("data-field");
+        this.update(userID, field, value);
+      }
+    });
 
-  form.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    const name = document.querySelector("#name").value;
-    const email = document.querySelector("#email").value;
-    const phone = document.querySelector("#phone").value;
-    const row = document.createElement("tr");
-    row.innerHTML = `
-        <td class="js-value">${name}</td>
-        <td class="js-value">${email}</td>
-        <td class="js-value">${phone}</td>
-        <td class="js-active-value">false</td>
-        <td><button class="js-button-activate activate">Activate</button><button class="js-button-remove">Remove</button></td>
+    this.form.addEventListener("submit", evt => {
+      evt.preventDefault();
+      const name = this.form.querySelector("#name").value;
+      const email = this.form.querySelector("#email").value;
+      const phone = this.form.querySelector("#phone").value;
+      this.add(name, email, phone);
+    });
+  }
+
+  add(name, email, phone) {
+    const id = Date.now() + "";
+    const user = {
+      id,
+      name,
+      email,
+      phone,
+      active: false
+    };
+    this.users.set(id, user);
+
+    this.appendRow(user);
+  }
+
+  remove(id) {
+    if (this.users.delete(id))
+      this.table.querySelector(`tr[data-id='${id}']`).remove();
+  }
+
+  update(id, field, value) {
+    const user = this.users.get(id);
+    user[field] = value;
+    this.updateRow(user);
+  }
+
+  toggleActivate(id) {
+    const user = this.users.get(id);
+    user.active = !user.active;
+    this.updateRow(user);
+  }
+
+  generateRow(data) {
+    const { id, name, email, phone, active } = data;
+    return `
+      <td class="js-value" data-field="name" data-id="${id}">${name}</td>
+      <td class="js-value" data-field="email" data-id="${id}">${email}</td>
+      <td class="js-value" data-field="phone" data-id="${id}">${phone}</td>
+      <td class="js-active-value" data-id="${id}">${active}</td>
+      <td>
+        <button class="js-button-activate" data-id="${id}">
+          ${active ? "Deactivate" : "Activate"}
+        </button>
+        <button class="js-button-remove" data-id="${id}">Remove</button>
+      </td>
     `;
-    tbody.appendChild(row);
-    form.reset();
-  });
-})();
+  }
+
+  updateRow(data) {
+    const row = this.table.querySelector(`tr[data-id='${data.id}']`);
+    row.innerHTML = this.generateRow(data);
+  }
+
+  appendRow(data) {
+    const row = document.createElement("tr");
+    row.setAttribute("data-id", data.id);
+    row.innerHTML = this.generateRow(data);
+    this.tbody.appendChild(row);
+    this.form.reset();
+  }
+}
+
+const form = document.querySelector("form");
+const table = document.querySelector("table");
+
+let users = new UsersList(form, table);
+users.add("John Doe", "johndoe@gmail.com", "+3 (455) 354-54-89");
